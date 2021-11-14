70dc9e827cf7e1468ba2cbf1019bed869dc52aa6
Youngseo <dudtj3116@naver.com>
2021-11-14 22:14:21 +0900

    09. 컴포넌트 스타일링

---
+++ 09/styling-react/src/App.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+import SassComponent from "./SassComponet";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <SassComponent />
+      </div>
+    );
+  }
+}
+
+export default App;
